579b32ec061922963bddf0774deaa7119a313f45
unknown <mohit80940@gmail.com>
2021-11-22 16:29:07 +0530

    dotenv

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const getAnalytics = require("firebase/analytics");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+var dotenv = require('dotenv').config();
 const LocalStrategy = require('passport-local').Strategy;
 
 const bcrypt = require('bcrypt');
